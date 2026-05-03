bce113ccba50ef51ae447f89c9704652bcae41d0
Reyleal360 <rexleal360@gmail.com>
2026-05-02 22:36:26 -0500

    Solución de la semana 3 (dia 1 y dia 3)

---
+++ semana2/semana3-dia3/seguimiento-comunitario.js
@@ -0,0 +1,11 @@
+let semanaActual = 1;
+const totalSemanas = 6;
+
+console.log("Inicio de seguimiento comunitario");
+
+while (semanaActual <= totalSemanas) {
+  console.log("Realizando seguimiento de la semana:", semanaActual);
+  semanaActual = semanaActual + 1;
+}
+
+console.log("Seguimiento comunitario finalizado.");
